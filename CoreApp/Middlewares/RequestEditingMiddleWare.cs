ad7f2bc9c00012ec574d9e5c20f3fe0ba7c4f94a
ismailfurkangonul <ismailfurkangonul2@gmail.com>
2022-08-06 19:54:22 +0300

    Add project files.

---
+++ CoreApp/Middlewares/RequestEditingMiddleWare.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CoreApp.Middlewares
+{
+    public class RequestEditingMiddleWare
+    {
+
+        private RequestDelegate _requestDelegate;
+
+        public  RequestEditingMiddleWare(RequestDelegate requestDelegate)
+        {
+
+            _requestDelegate = requestDelegate;
+
+        }
+        public async Task Invoke(HttpContext context)
+        {
+
+            if (context.Request.Path.ToString()=="/Furkan")
+            {
+                await context.Response.WriteAsync("hosgeldin mertens");
+
+
+            }
+            else
+            {
+               await _requestDelegate.Invoke(context);
+
+
+            }
+
+        }
+    }
+}
